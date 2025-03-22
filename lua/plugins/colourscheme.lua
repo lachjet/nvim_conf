c3071cb751ade0eee3b60fe7c647f720668d3c74
lachjet <102904523+lachjet@users.noreply.github.com>
2025-03-22 18:40:27 +1000

    fixed async bug in config

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -6,7 +6,18 @@ return {
       colorscheme = "tokyonight",
 	  style = "moon",
 	  transparent = true,
-
-    },
+	  styles = {
+		sidebars = "transparent",
+		floats = "transparent",
+	 },  
+  },
+	config = function()
+		vim.cmd([[colorscheme tokyonight-moon]])
+		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+	end,
   }
 
