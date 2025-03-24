9df72079e9b7e3ee52af3adb6f0db1bd1565c36b
lachjet <102904523+lachjet@users.noreply.github.com>
2025-03-24 15:19:41 +1000

    added keymap for opening just telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,13 +16,6 @@ map("n", "<leader>l", ":Lazy<CR>", opts)
 map("v", "<C-c>", '"+y', opts)
 map({"n", "i"}, "<C-p>", '"+p', opts)
 map("i", "<C-S-v>", "<C-r>+", opts)
-
--- Terminal navigation
-nmap("t", "<C-w><Left>", "<C-\\><C-n><C-w>h", opts)
-nmap("t", "<C-w><Down>", "<C-\\><C-n><C-w>j", opts)
-nmap("t", "<C-w><Up>", "<C-\\><C-n><C-w>k", opts)
-nmap("t", "<C-w><Right>", "<C-\\><C-n><C-w>l", opts)
-nmap("t", "<C-w>/", "<C-\\><C-n>", opts)
  
 -- Neotree
 map("n", "<leader>nt", ":Neotree toggle=true<CR>", opts)
@@ -33,7 +26,14 @@ map("n", "<leader>fo", ":Oil<CR>", opts)
 -- Telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+map("n", "<leader>ft", ":Telescope <CR>", opts)
 
+-- Terminal navigation
+nmap("t", "<C-w><Left>", "<C-\\><C-n><C-w>h", opts)
+nmap("t", "<C-w><Down>", "<C-\\><C-n><C-w>j", opts)
+nmap("t", "<C-w><Up>", "<C-\\><C-n><C-w>k", opts)
+nmap("t", "<C-w><Right>", "<C-\\><C-n><C-w>l", opts)
+nmap("t", "<C-t>/", "<C-\\><C-n>", opts)
 
 --floaterm
 map("n", "<leader>td", ":FloatermNew --wintype=split --height=0.3<CR>", opts)
@@ -43,8 +43,6 @@ map("t", "<C-t>k", "<C-\\><C-n>:FloatermKill<CR>", opts)
 map("t", "<C-t>n", "<C-\\><C-n>:FloatermNext<CR>", opts)
 map("t", "<C-t>h", "<C-\\><C-n>:FloatermHide<CR>", opts)
 map("t", "<C-t>a", "<C-\\><C-n>:FloatermNew<CR>", opts)
-
-
 
 --lazygit
 map("n", "<leader>g", ":FloatermNew --title=lazygit --width=150 --height=50 <CR>lazygit<CR>", opts)
