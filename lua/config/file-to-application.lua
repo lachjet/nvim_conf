7e29e0484a9811bbec44905137c94897a7c414e1
lachjet <102904523+lachjet@users.noreply.github.com>
2025-04-17 18:11:59 +1000

    simplified

--- lua/config/file-to-application.lua
+++ lua/config/file-to-application.lua
@@ -1,24 +1,36 @@
 local M = {}
 
-M.file_openers = {
-    pdf = function(path) vim.fn.jobstart({ "zathura", path }, { detach = true }) end,
-    png = function(path) vim.fn.jobstart({ "sxiv", path }, { detach = true }) end,
-    jpg = function(path) vim.fn.jobstart({ "sxiv", path }, { detach = true }) end,
-    jpeg = function(path) vim.fn.jobstart({ "sxiv", path }, { detach = true }) end,
-    gif = function(path) vim.fn.jobstart({ "sxiv", path }, { detach = true }) end,
-    html = function(path) vim.cmd("FloatermNew --title=preview.html --width=150 --height=50 w3m " .. path) end,
-    odt = function(path) vim.fn.jobstart({ "libreoffice", path }, { detach = true }) end,
-    doc = function(path) vim.fn.jobstart({ "libreoffice", path }, { detach = true }) end,
-    docx = function(path) vim.fn.jobstart({ "libreoffice", path }, { detach = true }) end,
+-- Map file extensions to applications
+M.file_applications = {
+    pdf = "zathura",
+    png = "sxiv",
+    jpg = "sxiv",
+    jpeg = "sxiv",
+    gif = "sxiv",
+    odt = "libreoffice",
+    doc = "libreoffice",
+    docx = "libreoffice",
+}
+
+-- Special handlers for formats needing more customization
+local special_handlers = {
+    html = function(path)
+        vim.cmd("FloatermNew --title=preview.html --width=150 --height=50 w3m " .. path)
+    end,
 }
 
 M.open_file = function(path)
     local ext = path:match("^.+%.([^.]+)$")
-    if ext and M.file_openers[ext] then
-        M.file_openers[ext](path)
+    if not ext then return false end
+
+    if special_handlers[ext] then
+        special_handlers[ext](path)
+    elseif M.file_applications[ext] then
+        vim.fn.jobstart({ M.file_applications[ext], path }, { detach = true })
     else
         return false
     end
+
     return true
 end
 
