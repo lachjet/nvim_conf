b9351586f2801852e09135160ed0f4010287c1ab
lachjet <102904523+lachjet@users.noreply.github.com>
2025-10-06 13:24:55 +1000

    wooo completion is back

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,65 +1,121 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/vim-vsnip",
-      "hrsh7th/cmp-vsnip",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      local cmp = require("cmp")
+	{
+		'hrsh7th/nvim-cmp',
+		event = "InsertEnter",
+		dependencies = {
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+			'hrsh7th/vim-vsnip',
+			'hrsh7th/cmp-vsnip',
+			'f3fora/cmp-spell', -- spell completion source
+			-- Note: removed 'hrsh7th/cmp-buffer' from global deps so buffer is not auto-used everywhere.
+		},
+		config = function()
+			local cmp = require('cmp')
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "vsnip" },   -- always safe
-          -- { name = "nvim_lsp" }  <-- attach dynamically
-        }, {
-          { name = "buffer" },
-        }),
-      })
+			-- Global default: no 'buffer' source here
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
+				}),
+				-- keep language server + snippets by default only
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'vsnip' },
+				}, {
+					{ name = 'path' }, -- optionally keep path in the secondary bucket
+				}),
+			})
 
-      -- Cmdline completion
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = { { name = "buffer" } },
-      })
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-      })
+			-- Cmdline completion for search (keep buffer here if you want search completions)
+			cmp.setup.cmdline({ '/', '?' }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = 'buffer' }
+				}
+			})
 
-      -- Setup LSP servers
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+			-- Cmdline completion for commands
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' }
+				}, {
+					{ name = 'cmdline' }
+				})
+			})
 
-      local servers = { "lua_ls", "pyright" } -- add your servers here
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            -- Attach LSP source only after server attaches
-            cmp.setup.buffer({ sources = { { name = "nvim_lsp" }, { name = "vsnip" } } })
-          end,
-        }
-      end
-    end,
-  }
+			-- ========== Filetype-specific setups ==========
+
+			-- LaTeX (and similar): enable spell + buffer + LSP + snippets
+			cmp.setup.filetype('tex', {
+				sources = cmp.config.sources({
+					{ name = 'spell' },      -- spelling suggestions
+					{ name = 'buffer' },     -- buffer completions allowed for tex
+					{ name = 'nvim_lsp' },
+					{ name = 'vsnip' },
+				})
+			})
+
+			-- Markdown and plain text: enable spell + buffer as well
+			for _, ft in ipairs({ 'markdown', 'text' }) do
+				cmp.setup.filetype(ft, {
+					sources = cmp.config.sources({
+						{ name = 'spell' },
+						{ name = 'buffer' },
+						{ name = 'nvim_lsp' },
+						{ name = 'vsnip' },
+					})
+				})
+			end
+
+			-- If you want buffer completions for specific programming filetypes (optional)
+			-- e.g. enable buffer completions for 'rust' and 'python'
+			--[[
+			for _, ft in ipairs({ 'rust', 'python' }) do
+				cmp.setup.filetype(ft, {
+					sources = cmp.config.sources({
+						{ name = 'nvim_lsp' },
+						{ name = 'buffer' },  -- allow buffer completions for these languages
+						{ name = 'vsnip' },
+					})
+				})
+			end
+			]]
+
+			-- Recommended: enable spell checking only for relevant filetypes
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "tex", "markdown", "text" },
+				callback = function()
+					vim.opt_local.spell = true
+					vim.opt_local.spelllang = { "en_us" } -- change to en_au if you prefer
+				end,
+			})
+		end,
+	}
 }
 
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,166 +1,153 @@
 return {
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		{ 'hrsh7th/nvim-cmp', opts = {} },
-	},
-	config = function()
-		local lspconfig = require('lspconfig')
-		local capbts   = require('cmp_nvim_lsp').default_capabilities()
+	{
+		'neovim/nvim-lspconfig',
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			'hrsh7th/cmp-nvim-lsp',
+		},
+		config = function()
+			local lspconfig = require('lspconfig')
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-		-- Optional: configure how diagnostics are displayed
-		vim.diagnostic.config({
-			virtual_text     = false,
-			signs            = true,
-			underline        = true,
-			update_in_insert = false,
-			severity_sort    = false,
-		})
+			-- Configure how diagnostics are displayed
+			vim.diagnostic.config({
+				virtual_text     = false,
+				signs            = true,
+				underline        = true,
+				update_in_insert = false,
+				severity_sort    = false,
+			})
 
-		-- on_attach maps keys once an LSP server is attached to a buffer
-		local on_attach = function(client, bufnr)
-			local buf_map = vim.api.nvim_buf_set_keymap
-			local opts    = { noremap = true, silent = true }
+			-- on_attach maps keys once an LSP server is attached to a buffer
+			local on_attach = function(client, bufnr)
+				local buf_map = vim.api.nvim_buf_set_keymap
+				local opts    = { noremap = true, silent = true }
 
-			-- Hover (view type/signature/docs) with K
-			buf_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+				-- Hover (view type/signature/docs) with K
+				buf_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
-			-- Jump to definition with gd
-			buf_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+				-- Jump to definition with gd
+				buf_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 
-			-- Open code action menu
-			buf_map(bufnr, 'n', "<Leader>c", "<cmd>lua vim.lsp.buf.code_action({apply = false})<CR>", opts)
+				-- Open code action menu
+				buf_map(bufnr, 'n', '<Leader>c', '<cmd>lua vim.lsp.buf.code_action({apply = false})<CR>', opts)
 
+				-- Navigate diagnostics
+				buf_map(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+				buf_map(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
-			-- Navigate diagnostics
-			buf_map(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-			buf_map(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+				-- Show diagnostics when <Leader>e is pressed
+				buf_map(bufnr, 'n', '<Leader>e', '<cmd>lua vim.diagnostic.open_float(nil, { focus = false })<CR>', opts)
 
-			-- Only show diagnostics when <Leader>e is pressed
-			buf_map(
-				bufnr,
-				'n',
-				'<Leader>e',
-				'<cmd>lua vim.diagnostic.open_float(nil, { focus = false })<CR>',
-				opts
-			)
+				-- Python-specific keybindings
+				if vim.bo[bufnr].filetype == 'python' then
+					buf_map(bufnr, 'n', '<Leader>re',
+						'<cmd>FloatermNew! --name=python3 --wintype=float --autoclose=0 python3; exit<CR>', opts)
+					buf_map(bufnr, 'n', '<Leader>rd',
+						'<cmd>FloatermNew! --name=python3 --wintype=split --position=bottom --height=15 python3; exit<CR>', opts)
+				end
 
-				if vim.bo[bufnr].filetype == "python" then
-				-- Floating Python3 (<Leader>re)
-				buf_map(bufnr, 'n', '<Leader>re',
-					'<cmd>FloatermNew! --name=python3 --wintype=float --autoclose=0 python3; exit<CR>', opts)
-
-				-- Docked Python3 (<Leader>rd)
-				buf_map(bufnr, 'n', '<Leader>rd',
-					'<cmd>FloatermNew! --name=python3 --wintype=split --position=bottom --height=15 python3; exit<CR>', opts)
-			end		-- Only for MATLAB files
-			if vim.bo[bufnr].filetype == "matlab" then
-				-- Floating MATLAB REPL (<Leader>re)
-				buf_map(bufnr, 'n', '<Leader>re',
-					'<cmd>FloatermNew! --name=MATLAB_REPL --wintype=float --autoclose=0 /usr/local/MATLAB/R2025a/bin/matlab -nodesktop nosplash; exit<CR>', opts)
-
-				-- Docked MATLAB REPL (<Leader>rd)
-				buf_map(bufnr, 'n', '<Leader>rd',
-					'<cmd>FloatermNew! --name=MATLAB_REPL --wintype=split --position=bottom --height=15 /usr/local/MATLAB/R2025a/bin/matlab -nodesktop -nosplash; exit<CR>', opts)
+				-- MATLAB-specific keybindings
+				if vim.bo[bufnr].filetype == 'matlab' then
+					buf_map(bufnr, 'n', '<Leader>re',
+						'<cmd>FloatermNew! --name=MATLAB_REPL --wintype=float --autoclose=0 /usr/local/MATLAB/R2025a/bin/matlab -nodesktop -nosplash; exit<CR>', opts)
+					buf_map(bufnr, 'n', '<Leader>rd',
+						'<cmd>FloatermNew! --name=MATLAB_REPL --wintype=split --position=bottom --height=15 /usr/local/MATLAB/R2025a/bin/matlab -nodesktop -nosplash; exit<CR>', opts)
+				end
 			end
 
-
-
-
-		end
-
-		lspconfig.clangd.setup({
-			on_attach  = on_attach,
-			capabilities = capbts,
-			cmd = {
-				'clangd',
-				'--background-index',
-				'--enable-config',
-				'--compile-commands-dir=build',
-			},
-			filetypes  = { 'c', 'cpp', 'objc', 'objcpp' },
-			root_dir   = lspconfig.util.root_pattern('.clangd'),
-		})
-
-		-- We want the root directory for neocmake to have BOTH a .clangd file 
-		-- and a MakeLists.txt file
-		local function and_root_dir(fname)
-			return lspconfig.util.search_ancestors(fname, function(path)
-				local has_clangd = lspconfig.util.path.is_file(lspconfig.util.path.join(path, ".clangd"))
-				local has_cmake  = lspconfig.util.path.is_file(lspconfig.util.path.join(path, "CMakeLists.txt"))
-				if has_clangd and has_cmake then
-					return path
-				end
-			end)
-		end
-
-		if not lspconfig.neocmake then
-			lspconfig.neocmake = {
-				default_config = {
-					cmd = vim.lsp.rpc.connect('127.0.0.1', '9257'),
-					filetypes = { "cmake" },
-					root_dir = and_root_dir,
-					single_file_support = true,
-					enable_external_cmake_lint = true;
-					on_attach = on_attach,
-					capabilities = capbts,
-					init_options = {
-						format = { enable = true },
-					},
+			-- Clangd setup
+			lspconfig.clangd.setup({
+				on_attach    = on_attach,
+				capabilities = capabilities,
+				cmd = {
+					'clangd',
+					'--background-index',
+					'--enable-config',
+					'--compile-commands-dir=build',
 				},
-			}
-		end
+				filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+				root_dir  = lspconfig.util.root_pattern('.clangd'),
+			})
 
-		lspconfig.neocmake.setup({})
+			-- Custom root directory function for neocmake
+			-- Requires BOTH .clangd and CMakeLists.txt
+			local function and_root_dir(fname)
+				return lspconfig.util.search_ancestors(fname, function(path)
+					local has_clangd = lspconfig.util.path.is_file(lspconfig.util.path.join(path, '.clangd'))
+					local has_cmake  = lspconfig.util.path.is_file(lspconfig.util.path.join(path, 'CMakeLists.txt'))
+					if has_clangd and has_cmake then
+						return path
+					end
+				end)
+			end
 
-		-- Lua language server
-		lspconfig.lua_ls.setup({
-			on_attach    = on_attach,
-			capabilities = capbts,
-		})
+			-- Neocmake setup
+			if not lspconfig.neocmake then
+				lspconfig.neocmake = {
+					default_config = {
+						cmd = vim.lsp.rpc.connect('127.0.0.1', '9257'),
+						filetypes = { 'cmake' },
+						root_dir = and_root_dir,
+						single_file_support = true,
+						enable_external_cmake_lint = true,
+						on_attach = on_attach,
+						capabilities = capabilities,
+						init_options = {
+							format = { enable = true },
+						},
+					},
+				}
+			end
+			lspconfig.neocmake.setup({})
 
-		-- LaTeX language server
-		lspconfig.texlab.setup({
-			on_attach    = on_attach,
-			capabilities = capbts,
+			-- Lua language server
+			lspconfig.lua_ls.setup({
+				on_attach    = on_attach,
+				capabilities = capabilities,
+			})
 
-		})
+			-- LaTeX language server
+			lspconfig.texlab.setup({
+				on_attach    = on_attach,
+				capabilities = capabilities,
+			})
 
-		-- MATLAB Language Server
-		lspconfig.matlab_ls.setup({
-			on_attach    = on_attach,
-			capabilities = capbts,
-			cmd = {
-				"node",
-				"/home/lachjet/MATLAB-language-server/out/index.js",
-				"--stdio",
-				"--matlabInstallPath=/usr/local/MATLAB/latest"
-			},
-			filetypes = { "matlab" },
-			root_dir = lspconfig.util.root_pattern(".git", "."),
-			handlers = {
-				["textDocument/publishDiagnostics"] = vim.lsp.with(
-					vim.lsp.diagnostic.on_publish_diagnostics, {
-						virtual_text = true,  -- inline errors/warnings
-						signs = true,         -- gutter signs
-						underline = true,
-						update_in_insert = false,
-					}
-				),
-			}
-		})
+			-- MATLAB language server
+			lspconfig.matlab_ls.setup({
+				on_attach    = on_attach,
+				capabilities = capabilities,
+				cmd = {
+					'node',
+					'/home/lachjet/MATLAB-language-server/out/index.js',
+					'--stdio',
+					'--matlabInstallPath=/usr/local/MATLAB/latest'
+				},
+				filetypes = { 'matlab' },
+				root_dir = lspconfig.util.root_pattern('.git', '.'),
+				handlers = {
+					['textDocument/publishDiagnostics'] = vim.lsp.with(
+						vim.lsp.diagnostic.on_publish_diagnostics, {
+							virtual_text = true,
+							signs = true,
+							underline = true,
+							update_in_insert = false,
+						}
+					),
+				}
+			})
 
-		lspconfig.pylsp.setup({
-			on_attach	= on_attach,
-			capabilities = capbts,
-		})
+			-- Python language server
+			lspconfig.pylsp.setup({
+				on_attach    = on_attach,
+				capabilities = capabilities,
+			})
 
-
-
-		-- Optional: customize diagnostic signs
-		vim.fn.sign_define("DiagnosticSignError", {text = "✖", texthl = "DiagnosticError"})
-		vim.fn.sign_define("DiagnosticSignWarn",  {text = "⚠", texthl = "DiagnosticWarn"})
-		vim.fn.sign_define("DiagnosticSignInfo",  {text = "ℹ", texthl = "DiagnosticInfo"})
-		vim.fn.sign_define("DiagnosticSignHint",  {text = "➤", texthl = "DiagnosticHint"})
-	end,
+			-- Customize diagnostic signs
+			vim.fn.sign_define('DiagnosticSignError', { text = '✖', texthl = 'DiagnosticError' })
+			vim.fn.sign_define('DiagnosticSignWarn',  { text = '⚠', texthl = 'DiagnosticWarn' })
+			vim.fn.sign_define('DiagnosticSignInfo',  { text = 'ℹ', texthl = 'DiagnosticInfo' })
+			vim.fn.sign_define('DiagnosticSignHint',  { text = '➤', texthl = 'DiagnosticHint' })
+		end,
+	}
 }
-
